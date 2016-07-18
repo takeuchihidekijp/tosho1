93a7eaac8f6ec2d52617537965845559cb6fc89f
Yoji Okada <yokada.moo.jp@gmail.com>
2016-07-19 00:28:21 +0900

    iine 1

--- app/controllers/iine_controller.rb
+++ app/controllers/iine_controller.rb
@@ -1,8 +1,23 @@
 class IineController < ApplicationController
   before_action :logged_in_user
-  
+
   def new
     @iine = DocRef.new
   end
+
+  def create
+    o = {
+      from: params[:from],
+      to: params[:to]
+    }
+    doc = Doc.find(params[:doc_id])
+    doc.doc_refs.create(o)
+  end
+
+  def destroy
+  end
+
+  private
+
 end
 
